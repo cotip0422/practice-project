854d613cc911113abf1fcd54be26cb3862c93560
cotip0422 <cotip0422@gmail.com>
2021-06-29 01:52:17 +0900

    commit2

--- gittest.py
+++ gittest.py
@@ -1,3 +1,115 @@
-print("hello world")
-a = 1
-print(a)
+from time import sleep
+
+print("환영합니다!")
+sleep(0.5)
+print("로그인을 해주십시오")
+
+Login_Id = input("아이디>")
+
+if Login_Id in "love05uni":
+    print("비밀 번호를 입력하십시오")
+else:
+    print("다시시도 하십시오")
+    exit(1)
+
+sleep(0.5)
+
+Login_PW = input("페스워드>")
+
+if Login_PW in "tjddus0422":
+    print("로그인 되었습니다!")
+else:
+    print("다시시도 하십시오")
+    exit(1)
+
+sleep(1)
+for i in range(5):
+    print("정보 로딩중")
+for i in range(6):
+    sleep(0.6)
+    print("리소스 확인중")
+    sleep(0.5)
+Re_dow = input("다운받을 리소스가 있습니다(다운 받으시겠습니까?)>")
+
+if Re_dow in "네":
+    print("리소스 다운중(1/5GB)")
+    sleep(0.5)
+    print("리소스 다운중(2/5GB)")
+    sleep(0.5)
+    print("리소스 다운중(3/5GB)")
+    sleep(0.5)
+    print("리소스 다운중(4.23/5GB)")
+    sleep(0.5)
+    print("리소스 다운중(4.52/5GB)")
+    sleep(0.5)
+    print("리소스 다운중(4.64/5GB)")
+    sleep(0.5)
+    print("리소스 다운중(4.91/5GB)")
+    sleep(0.5)
+    print("리소스 다운중(5/5GB)")
+    sleep(1)
+    for i in range(10):
+        print("리소스 확인중")
+        sleep(0.5)
+    print("완료!")
+else:
+    print("취소되었습니다")
+    exit(1)
+sleep(1)
+print("sudo apt-get intall code.pack")
+print("password>")
+sleep(1)
+print("/ sudo - ubuntu - main - password")
+sleep(0.8)
+print(".")
+sleep(0.8)
+print(".")
+sleep(1)
+print("sudo password not found")
+print("reset sudo password")
+SudoPassword = input("NEW PASSWORD>")
+print("reset")
+sleep(0.8)
+print(".")
+sleep(0.8)
+print(".")
+sleep(0.8)
+print(".")
+print("{complete} [and restart computer]")
+sleep(2)
+print("restart computer")
+sleep(0.8)
+print(".")
+sleep(0.8)
+print(".")
+sleep(0.8)
+print(".")
+print("ASUS COMPUTER")
+print("Hello! New user")
+sleep(1)
+print("sudo apt-get intall code.pack")
+pwsudo = input("password>")
+if pwsudo in "ksy0422":
+    print("installing")
+    sleep(3)
+else:
+    print("wrong password")
+    exit(1)
+for i in reversed([1, 2, 3, 4, 5, 6, 7, 8]):
+    print({}, "data find".format(i))
+    sleep(1)
+print("main boot os select")
+print("not many boot os")
+print("will be automatically selected")
+windows = 1
+for a in "windows ubuntu":
+    print(a)
+    sleep(1)
+print("환영합니다!")
+print("초기 설정을 시작합니다")
+sleep(1)
+lang = input("언어를 입력해주세요>")
+print(lang, "으로 설정합니다")
+print("위치 정보를 확인하시겠습니까?")
+gps = input("GPS ON>")
+print(gps, "로 설정되었습니다 [no yes 네 아니요 ㅇ ㄴㄴ 가 아닐경우 설정하지 않은것으로 간주됩니다]")
